ed2f3a33a982f0680c8dce615c88a19f3b77467d
npmzero <surajpathak3430@gmail.com>
2025-02-20 20:32:52 +0545

    Home Page Part

---
+++ Models/ViewModel/DashboardVM.cs
@@ -0,0 +1,8 @@
+﻿namespace ecom.Models.ViewModel
+{
+    public class DashboardVM
+    {
+        public List<Category> categoryinfo { get; set; }
+        public List<ProductItem> productitems { get; set; }
+    }
+}
