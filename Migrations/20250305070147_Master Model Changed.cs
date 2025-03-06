e17be08535c59effdcadfa75327092072037fe1e
npmzero <surajpathak3430@gmail.com>
2025-03-06 09:03:57 +0545

    new updated gitignore

---
+++ Migrations/20250305070147_Master Model Changed.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecom.Migrations
+{
+    /// <inheritdoc />
+    public partial class MasterModelChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentStatus",
+                table: "ProductOrderMaster",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionId",
+                table: "ProductOrderMaster",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "ProductOrderMaster");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionId",
+                table: "ProductOrderMaster");
+        }
+    }
+}
